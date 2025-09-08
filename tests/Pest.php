9dd2bce29698989930b35b3f52ce3f494e4426b5
Arbi Kullakshi <arbikullakshi0@gmail.com>
2025-09-08 13:49:20 +0200

    feat: refactor SMS message handling and add logging capabilities
    scrap tests, were not testing actual logic

--- tests/Pest.php
+++ tests/Pest.php
@@ -2,16 +2,4 @@
 
 namespace MagicSystemsIO\Notifyre\Tests;
 
-pest()->extend(TestCase::class)
-    ->in('Feature');
-
-
-expect()->extend('toBeOne', function () {
-    return $this->toBe(1);
-});
-
-
-function something()
-{
-    // ..
-}
+pest()->extend(TestCase::class)->in('Feature');
